c536a29173d3cf311ee176e360140025a64ea072
fmb2021 <gerardo.mendoza@fmbsolutions.mx>
2024-02-09 00:25:19 -0600

    Buttons and Containers

---
+++ lib/presentation/widgets/shared/video_player/video_buttons.dart
@@ -0,0 +1,67 @@
+import 'package:flutter/material.dart';
+import 'package:toktik/config/helpers/human_formats.dart';
+import 'package:toktik/domain/entities/video_post.dart';
+import 'package:animate_do/animate_do.dart';
+
+class VideoButtons extends StatelessWidget {
+  final VideoPost video;
+
+  const VideoButtons({super.key, required this.video});
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        _CustomIconButton(
+            value: video.likes,
+            iconData: Icons.favorite,
+            iconColor: Colors.red),
+        const SizedBox(height: 20),
+        _CustomIconButton(
+            value: video.views,
+            iconData: Icons.remove_red_eye_outlined,
+            iconColor: Colors.brown),
+        const SizedBox(height: 20),
+        SpinPerfect(
+          infinite: true,
+          duration: Duration(seconds: 4),
+          child: 
+            const _CustomIconButton( 
+              value: 0, 
+              iconData: Icons.play_circle_outlined
+            ),
+        ),
+      ],
+    );
+  }
+}
+
+class _CustomIconButton extends StatelessWidget {
+  final int value;
+  final IconData iconData;
+  final Color? color;
+
+  const _CustomIconButton(
+      {required this.value, required this.iconData, iconColor})
+      : color = iconColor ?? Colors.white;
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        IconButton(
+            onPressed: () {},
+            icon: Icon(
+              iconData,
+              color: color,
+              size: 50,
+            )),
+        if( value > 0)
+        Text(HumanFormats.humanReadbleNumber(value.toDouble()),
+            style: const TextStyle(fontSize: 25)
+        )
+        // Text('$value' + '00 M', style: const TextStyle(fontSize: 25) ),
+      ],
+    );
+  }
+}
